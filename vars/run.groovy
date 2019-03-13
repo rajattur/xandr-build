ad7cb2e01c9a092534232ef1e685af5bca622302
kr367s <kr367s@csp.att.com>
2019-03-13 08:42:31 -0500

    updated jenkins file for xandr project

--- vars/run.groovy
+++ vars/run.groovy
@@ -1,23 +1,62 @@
 import com.att.xandr.Constants
 
 def call() {
-    node() {
-        stage('Checkout') {
-            git branch: 'feature/jenkins-module', credentialsId: 'xandr-git', url: 'https://kr367s@codecloud.web.att.com/scm/st_addco/audience_builder.git'
-        }
-        stage('Build') {
-            echo "Building..."
-            def constants = new Constants()
-            def aws_region = constants.getDockerRegistry()
-            println(aws_region.aws_region)
-            withEnv(["S3_URL=${params.get("s3_bucket")}"]) {
-                sh '''
-                    echo "${S3_URL}"
-                    ls -al && \
-                    cd audiencebuilder-gui && \
-                    ls -al
-                '''        
+    pipeline {
+        agent any
+        // agent {
+        //     label "audience-builder"
+        // }
+
+        stages {
+            stage('Main') {
+                steps {
+                    script {
+
+                        stage('Checkout') {
+                            if (env.BRANCH_NAME) {
+                                checkout scm
+                            } else {
+                                println 'Skipping checkout as SCM is not configured'
+                            }
+                        }
+
+                        stage('Build') {
+                            echo "Building..."
+                            def constants = new Constants()
+                            def aws_region = constants.getDockerRegistry()
+                            println(aws_region.aws_region)
+                            withEnv(["S3_URL=${params.get("s3_bucket")}"]) {
+                                sh '''
+                                    echo "${S3_URL}"
+                                    ls -al && \
+                                    cd audiencebuilder-gui && \
+                                    ls -al
+                                '''        
+                            }
+                        }
+
+                    }
+                }
             }
         }
     }
+    // node() {
+    //     stage('Checkout') {
+    //         git branch: 'feature/jenkins-module', credentialsId: 'xandr-git', url: 'https://kr367s@codecloud.web.att.com/scm/st_addco/audience_builder.git'
+    //     }
+    //     stage('Build') {
+    //         echo "Building..."
+    //         def constants = new Constants()
+    //         def aws_region = constants.getDockerRegistry()
+    //         println(aws_region.aws_region)
+    //         withEnv(["S3_URL=${params.get("s3_bucket")}"]) {
+    //             sh '''
+    //                 echo "${S3_URL}"
+    //                 ls -al && \
+    //                 cd audiencebuilder-gui && \
+    //                 ls -al
+    //             '''        
+    //         }
+    //     }
+    // }
 }

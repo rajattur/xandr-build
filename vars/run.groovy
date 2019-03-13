364dceb3360bb1f248ca4ae7c984692b1bc110b7
kr367s <kr367s@csp.att.com>
2019-03-12 23:15:59 -0500

    updated run.groovy file

--- vars/run.groovy
+++ vars/run.groovy
@@ -3,5 +3,8 @@ def call() {
         stage('Checkout') {
             git branch: 'feature/jenkins-module', credentialsId: 'xandr-git', url: 'https://kr367s@codecloud.web.att.com/scm/st_addco/audience_builder.git'
         }
+        stage('Build') {
+            echo "Building..."
+        }
     }
 }
